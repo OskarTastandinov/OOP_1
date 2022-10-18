6bf36d29144648273a3af68af42867157ad43dfe
askar <tashtandinov.oskar@yandex.ru>
2022-10-18 11:21:29 +0700

    Коммит ООП

---
+++ src/Human.java
@@ -0,0 +1,37 @@
+import javax.swing.plaf.synth.ColorType;
+
+public class Human {
+    int yearOfBirth;
+    String name;
+    String town;
+    String job;
+
+    public Human(String name, String town, int yearOfBirth, String job) {
+        if (name != name || name == null || name == "") {
+            this.name = null;
+        }else{
+            this.name = name;
+        }
+        if (yearOfBirth < 0){
+            this.yearOfBirth = Math.abs(yearOfBirth);
+        }else {
+            this.yearOfBirth = yearOfBirth;
+        }
+        if (town != town || town == null || town == ""){
+            this.town = null;
+        }else{
+            this.town = town;
+        }
+        if (job != job || job == null || job == ""){
+            this.job = null;
+        }else{
+            this.job = job;
+        }
+    }
+
+    public void human(){
+        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town +
+        ". Мне " +  yearOfBirth + " лет. Я работаю на должности " + job + ". Будем знакомы!");
+    }
+}
+
