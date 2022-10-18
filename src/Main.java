0a912417d552cedea54a821cfa077f2c87b3762b
askar <tashtandinov.oskar@yandex.ru>
2022-10-18 11:26:59 +0700

    Коммит ООП

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        Human maxim = new Human("Максим","Минск", 35, "Бренд менеджера");
-        maxim.human();
-
-        Human anna = new Human("Аня","Москва", 29, "методиста образовательных программ");
-        anna.human();
-
-        Human katya = new Human("Катя","Калининград", 28, "продакт-менеджера");
-        katya.human();
-
-        Human artem = new Human("Артем","Москва", 27, "директора по развитию бизнеса");
-        artem.human();
+        System.out.println("dgfgg");
 
     }
 
